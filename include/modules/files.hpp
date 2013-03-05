ebb338b4ea682880ad874c5fa10f8d96c4680d70
Jon Morehouse <morehousej09@gmail.com>
2013-03-04 19:07:36 -0800

    Added include folder

---
+++ include/modules/files.hpp
@@ -0,0 +1,20 @@
+#ifndef FILES_HPP
+#define FILES_HPP
+/*
+	This namespace will be responsible for reading / writing files
+	Will be responsible for pulling in proper file data
+	Can later be a templated function namespace etc
+
+*/
+namespace files {
+
+	extern int test;
+
+
+
+
+
+
+}
+
+#endif
---
+++ src/modules/files.cpp
@@ -0,0 +1,16 @@
+// include project depenendencies
+
+// #include "modules/files.hpp"
+#include "/Users/MorehouseJ09/Documents/courses/itp_499/assignments/assignment_1/include/modules/files.hpp"
+
+namespace files {
+
+	int test = 4;
+
+
+
+
+
+
+
+}
